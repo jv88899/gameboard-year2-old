73ad5afd01aa1433f357a3ad973d41bd2dd420f7
jv88899 <jv88899@gmail.com>
2020-10-05 10:44:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-year2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-year2)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Original from "./transactions/Original";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <Original companyName="Medica"/>
+    </div>
+  );
+}
